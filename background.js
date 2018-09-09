1d9505eef39c361e6e904d42727e64a5c0426f15
Nizar Mahmoud <nizarmah@hotmail.com>
2018-09-10 00:43:04 +0300

    Created Chrome Extension

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.browserAction.onClicked.addListener((tab) => {
+    console.log(tab.url.search("slader.com/cheatsheet/"));
+    if (tab.url.search("slader.com/cheatsheet/") > -1) {
+        chrome.tabs.insertCSS(
+            {
+                file: 'main.css'
+            }, 
+            () => {
+                chrome.tabs.executeScript({
+                    "code": "window.print();"
+                });
+            }
+        );
+    }
+});
